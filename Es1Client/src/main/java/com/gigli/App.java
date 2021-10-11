cf54fbdf809d4e742fa019716201e6a89594ac42
fiippogigli <76041375+fiippogigli@users.noreply.github.com>
2021-10-11 19:26:00 +0200

    Add files via upload

---
+++ Es1Client/src/main/java/com/gigli/App.java
@@ -0,0 +1,17 @@
+package com.gigli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ClientStr cliente = new ClientStr();
+        cliente.connetti();
+        cliente.comunica();
+
+        
+    }
+}
---
+++ Es1Server/src/main/java/com/gigli/App.java
@@ -0,0 +1,9 @@
+package com.gigli;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ServerStr servente = new ServerStr();
+        servente.attendi();
+        servente.comunica();
+    }
+}
